4bf14f9595114d9b09c5cd47daa67de7c93b13d9
Afnan Amiludin <amldn.afnn@gmail.com>
2022-10-21 00:12:04 +0700

    Deploy to heroku and database in azzure update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,7 +71,7 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password', 'role_name'))) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'message' => 'Invalid login details'
             ], 401);
